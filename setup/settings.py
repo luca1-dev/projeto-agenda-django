34fce37bb793c449aea27c5ce543b8e1a61cec6c
Lucas Peres <luuca1g3@gmail.com>
2025-10-21 17:45:13 -0300

    Corrigindo configuração de arquivos estaticos.

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,9 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'base_static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static',
+    os.path.join(BASE_DIR, 'base_static'),
+    
 ]
 
 # Default primary key field type
