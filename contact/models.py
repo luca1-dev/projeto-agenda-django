6446bd03815a9578ef8c0d71674e9885d4359d72
Lucas Peres <luuca1g3@gmail.com>
2025-10-22 11:12:07 -0300

    Criando e configurando model Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    age = models.IntegerField(blank=True)
+    phone_number = models.CharField(max_length=15)
+    date_created = models.DateTimeField(default=timezone.now)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
