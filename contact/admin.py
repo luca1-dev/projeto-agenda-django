6a3f7c8b344cc5a7193e6a93e89691ffedff6d50
Lucas Peres <luuca1g3@gmail.com>
2025-10-23 13:54:18 -0300

    Corrigindo a configuração do model Contact

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from contact import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'category',)
+    search_fields = ('first_name', 'category', 'phone_number',)
+    list_per_page = 30
+    list_max_show_all = 300
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Catergory'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -9,6 +19,10 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=15)
     date_created = models.DateTimeField(default=timezone.now)
     email = models.EmailField()
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, null=True, upload_to='picture/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
